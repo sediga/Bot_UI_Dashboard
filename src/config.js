59976aedda2e9e783031e4a4ad0c2a4101f1dd13
esgoud08 <sreenivasa.goud@gmail.com>
2025-11-17 09:06:38 -0600

    moved to app service

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://botflowsdashboardapi-app.wittystone-18932ede.southcentralus.azurecontainerapps.io',
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://flowtradashboardapi-g9a3e7ggbgahbpfr.westus2-01.azurewebsites.net',
   agentServerUrl: import.meta.env.VITE_AGENT_SERVER_URL || 'http://localhost:8000',
   apiKey: import.meta.env.VITE_API_KEY || 'u42Q7gXgVx8fN1rLk9eJ0cGm5wYzA2dR',
   googleClientId: "955813006145-uu954kodvgkvbf4g3itjem54inaobtlj.apps.googleusercontent.com"
